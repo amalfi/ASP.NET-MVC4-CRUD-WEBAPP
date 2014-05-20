75a7c187742e6609c9e5a2bbabe108633519c077
amalfi <mberendt546@gmail.com>
2014-05-20 22:51:18 +0200

    first comit

---
+++ ASP.NET MVC4 CRUD WEBAPP/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC4_CRUD_WEBAPP
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP.NET MVC4 CRUD WEBAPP/Models/Auction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP.NET_MVC4_CRUD_WEBAPP.Models
+{
+    public class Auction
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public decimal StartPrice { get; set; }
+        public decimal? CurrentPrice { get; set; }
+
+
+    }
+}
