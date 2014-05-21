ac71dfd1429b42bdf4c60f56ef1456bb63b5378d
amalfi <mberendt546@gmail.com>
2014-05-21 23:46:04 +0200

    _AuctionTile

--- ASP.NET MVC4 CRUD WEBAPP/Controllers/AuctionController.cs
+++ ASP.NET MVC4 CRUD WEBAPP/Controllers/AuctionController.cs
@@ -13,7 +13,35 @@ namespace ASP.NET_MVC4_CRUD_WEBAPP.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var auctions = new[]{
+                new Models.Auction()
+                {
+                    Title="Example Auction #1",
+                    Description= "This is an example Auction",
+                    StartTime=DateTime.Now,
+                    EndTime=DateTime.Now.AddDays(7),
+                    StartPrice =1.00m,
+                    CurrentPrice=null,
+                },
+                new Models.Auction()
+                {
+                    Title="Example Auction #2",
+                    Description="This is a second Auction",
+                    StartTime = DateTime.Now,
+                    EndTime=DateTime.Now.AddDays(7),
+                    StartPrice=1.00m,
+                    CurrentPrice=30m,
+                }
+
+            };
+
+            return View(auctions);
+        }
+
+        public ActionResult TempDataDemo()
+        {
+            TempData["SuccessMessage"] = "The action succeeded!";
+            return RedirectToAction("Index");
         }
 
         public ActionResult Auction()
